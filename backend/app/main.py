4e42b8b04e3596a3ded6909efc069fbca2187a72
tomaszbk-pc <zubik.tomas@gmail.com>
2024-04-28 11:01:08 -0300

    feat: :sparkles: starlette admin implementation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
-import psycopg2
-from app.make_llm_query import submit_prompt
-from app.models import Prompt
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette_admin.contrib.sqla import Admin, ModelView
+
+from app.models import Order, OrderDetail, Product, User, create_db_and_tables, engine
 
 app = FastAPI()
+
+create_db_and_tables()
+admin = Admin(engine, title="Example: SQLAlchemy")
 
 
 app.add_middleware(
@@ -15,54 +18,9 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+admin.add_view(ModelView(Product))
+admin.add_view(ModelView(User))
+admin.add_view(ModelView(Order))
+admin.add_view(ModelView(OrderDetail))
 
-@app.post("/retrieve-products")
-async def retrieve_products(prompt: Prompt):
-    query = submit_prompt(prompt.content)
-    print(query)
-    products = query_products(query)
-    return {"products": products}
-
-
-@app.post("/make_query")
-def query_db(query: str):
-    connection = psycopg2.connect(
-        host=host, user=user, password=password, database=database
-    )
-    cursor = connection.cursor()
-
-    cursor.execute(query)
-
-    results = cursor.fetchall()
-
-    cursor.close()
-    connection.close()
-
-    return results
-
-
-host = "ecommerce-database.cl0aei0o857j.us-west-2.rds.amazonaws.com"
-user = "postgres"
-password = "postgres"
-database = "laptops_database"
-
-
-def query_products(query):
-    connection = psycopg2.connect(
-        host=host, user=user, password=password, database=database
-    )
-    try:
-        cursor = connection.cursor()
-
-        cursor.execute(query.replace(";", " LIMIT 8;"))
-
-        products = cursor.fetchall()
-
-        cursor.close()
-        connection.close()
-
-        return products
-
-    except Exception as e:
-        print(f"error {e}")
-        return []
+admin.mount_to(app)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,60 @@
 from pydantic import BaseModel
+from sqlmodel import Field, Relationship, SQLModel, create_engine
+
+engine = create_engine("sqlite:///test.db", connect_args={"check_same_thread": False})
 
 
 class Prompt(BaseModel):
     content: str
+
+
+# SQL MODEL
+class Product(SQLModel, table=True):
+    __tablename__ = "products"
+    id: int = Field(primary_key=True)
+    name: str
+    description: str
+    price: float
+    stock: bool
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: int = Field(primary_key=True)
+    name: str
+    email: str
+    hashed_password: str
+    is_admin: bool
+
+    orders: list["Order"] = Relationship(back_populates="user")
+
+
+class Order(SQLModel, table=True):
+    __tablename__ = "orders"
+
+    id: int = Field(primary_key=True)
+    user_id: int = Field(foreign_key="users.id")
+    order_date: str
+    quantity: int
+    total: float
+
+    order_details: list["OrderDetail"] = Relationship(back_populates="order")
+    user: User = Relationship(back_populates="orders")
+
+
+class OrderDetail(SQLModel, table=True):
+    __tablename__ = "order_details"
+
+    id: int = Field(primary_key=True)
+    order_id: int = Field(foreign_key="orders.id")
+    product_id: int = Field(foreign_key="products.id")
+    quantity: int
+    total: float
+
+    product: Product = Relationship()
+    order: Order = Relationship(back_populates="order_details")
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
